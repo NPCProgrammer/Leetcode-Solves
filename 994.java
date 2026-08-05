af043999aa83a5e4ef0a15ab0891dcd95e1b2f42
Kearnan Bishop <61856526+NPCProgrammer@users.noreply.github.com>
2026-08-05 16:38:40 +0000

    994

---
+++ 994.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Set<String> fresh = new HashSet<>();
+        Set<String> rotten = new HashSet<>();
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    fresh.add("" + i + j);
+                }
+                else if (grid[i][j] == 2) {
+                    rotten.add("" + i + j);
+                }
+            }
+        }
+        int mins = 0;
+        int[][] dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        
+        while(fresh.size() > 0) {
+            Set<String> infected = new HashSet<>();
+            
+            for (String s : rotten) {
+                int i = s.charAt(0) - '0'; // converts to integer
+                int j = s.charAt(1) - '0';
+                for (int[] dir : dirs) {
+                    int nextI = i + dir[0];
+                    int nextJ = j + dir[1];
+                    if (fresh.contains("" + nextI + nextJ)) {
+                        fresh.remove("" + nextI + nextJ);
+                        infected.add("" + nextI + nextJ);
+                    }
+                }
+            }
+            if (infected.size() == 0) {
+                return -1;
+            }
+            rotten = infected;
+            mins++;
+        }
+        
+        return mins;
+        
+    }
+}
