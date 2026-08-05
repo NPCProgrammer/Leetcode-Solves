e4fdebd86676a326bef237a450b5a4d8b7341387
Kearnan Bishop <61856526+NPCProgrammer@users.noreply.github.com>
2026-08-05 22:14:07 +0000

    7

---
+++ 7.cpp
@@ -0,0 +1,16 @@
+// 8/5/2026
+
+#include <climits>
+class Solution {
+public:
+    int reverse(int x) {
+        int temp = x;
+        x=0;
+        int digit;
+        while (temp != 0 && ((x > 0 && (INT_MAX - x) > 0) || (x<0 && (INT_MIN+x) < 0))) {
+            digit = temp % 10;
+            x = x*10+digit;
+        }
+        return x;
+    }
+};
