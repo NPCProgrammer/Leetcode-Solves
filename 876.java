54c223f799c776372c70d831ceabf6308b8c945c
Kearnan Bishop <61856526+NPCProgrammer@users.noreply.github.com>
2026-08-05 16:35:33 +0000

    876

---
+++ 876.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        int count = 0;
+        while (slow != null) {
+            count++;
+            slow = slow.next;
+        }
+        count = count / 2;
+        slow = head;
+        while (count-->0) {
+            slow = slow.next;
+        }
+        return slow;
+    } 
+}
