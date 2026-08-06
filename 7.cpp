b03a37a8aff3155799ea74aa0282eb702ee93714
Kearnan Bishop <61856526+NPCProgrammer@users.noreply.github.com>
2026-08-06 17:56:17 +0000

    7 done

--- 7.cpp
+++ 7.cpp
@@ -1,4 +1,3 @@
-// 8/5/2026
 #include <iostream>
 #include <string>
 #include <climits>
@@ -7,22 +6,43 @@ public:
     int reverse(int x) {
         int temp = x;
         int permtemp = x;
+        int counter = 0;
         x=0;
         int digit;
-        while (temp != 0 && ((temp > 0 && (INT_MAX - temp) > 0) || (temp<0 && (INT_MIN+temp) < 0))) {
+        while (temp != 0 && ((temp > 0 && (INT_MAX - temp) > 0)
+                 || (temp<0 && (INT_MIN-temp) < 0))) {
             digit = temp % 10;
+            if ((x > INT_MAX/10) || (x == INT_MAX/10 && digit > 7) || ((x < INT_MIN/10) || (x == INT_MIN/10 && digit > 8))) {
+                  return 0;
+            }
+            std::cout << "digit: " << digit << std::endl;
             x = x*10+digit;
             temp = temp / 10;
         }
         return x;
     }
+};        
 
-};
+
+
+
+
+
+
+
+
+
+
+//2147483647= max
+// 7463847412
+//1534236469=  x (number we are reversing current progress)
+// if the currently reversed number gets bigger than INT_MAX/10 we know
+// its gonna overflow, same with being under INT_MIN/10
 
 int main() {
         Solution sol;
 
-        std::cout << sol.reverse(123) << std::endl;
+        std::cout << sol.reverse(1463847412) << std::endl;
 
         return 0;
     }
